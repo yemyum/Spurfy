7c593516ed3a36ba695627b28f7ad39019ad260a
yemyum <oyl582@naver.com>
2025-04-25 17:40:50 +0900

    Initial commit

---
+++ src/main/java/com/example/my_site/MySiteApplication.java
@@ -0,0 +1,14 @@
+package com.example.my_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySiteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySiteApplication.class, args);
+	}
+
+}
