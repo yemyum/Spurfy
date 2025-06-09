9a7c912fac9e0e6c6fc6d0603c2dd2b62dea46ab
yemyum <oyl582@naver.com>
2025-06-09 21:33:34 +0900

    CustomException + ErrorCode 구조 도입으로 삭제!

--- src/main/java/com/example/oyl/exception/ReservationValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.oyl.exception;
-
-public class ReservationValidationException extends RuntimeException{
-    public ReservationValidationException(String message) {
-        super(message);
-    }
-}
